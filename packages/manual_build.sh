f670b4712a0c860700abaac7488f19a612713424
David Albert <davidbalbert@gmail.com>
2025-01-03 18:51:34 -0500

    More WIP, broken

--- packages/manual_build.sh
+++ packages/manual_build.sh
@@ -1,32 +1,34 @@
-BOOTSTRAP_PREFIX="/tmp/gccbuild/out-bootstrap"
-PREFIX="/tmp/gccbuild/out"
 TARGET="aarch64-unknown-linux-gnu"
+STAGE1_PREFIX="/tmp/gccbuild/out-stage1"
+STAGE2_PREFIX="/tmp/gccbuild/out-stage2"
+STAGE2_SYSROOT="$STAGE2_PREFIX/$TARGET/sysroot"
+PREFIX="/tmp/gccbuild/out"
 SYSROOT="$PREFIX/$TARGET/sysroot"
 
 mkdir build-binutils
 cd build-binutils
-../binutils-2.37/configure --prefix=$BOOTSTRAP_PREFIX --with-sysroot=$SYSROOT --target=$TARGET --enable-new-dtags
+../binutils-2.37/configure --prefix="$STAGE1_PREFIX" --with-sysroot="$STAGE2_SYSROOT" --target="$TARGET" --program-prefix="$TARGET-" --enable-new-dtags
 make -j10
 make install
 cd ..
 
 mkdir build-gmp
 cd build-gmp
-../gmp-6.3.0/configure --prefix=$BOOTSTRAP_PREFIX
+../gmp-6.3.0/configure --prefix="$STAGE1_PREFIX"
 make -j10
 make install
 cd ..
 
 mkdir build-mpfr
 cd build-mpfr
-../mpfr-4.2.1/configure --prefix=$BOOTSTRAP_PREFIX --with-gmp=$BOOTSTRAP_PREFIX
+../mpfr-4.2.1/configure --prefix="$STAGE1_PREFIX" --with-gmp="$STAGE1_PREFIX"
 make -j10
 make install
 cd ..
 
 mkdir build-mpc
 cd build-mpc
-../mpc-1.3.1/configure --prefix=$BOOTSTRAP_PREFIX --with-gmp=$BOOTSTRAP_PREFIX
+../mpc-1.3.1/configure --prefix="$STAGE1_PREFIX" --with-gmp="$STAGE1_PREFIX"
 make -j10
 make install
 cd ..
@@ -34,9 +36,9 @@ cd ..
 mkdir build-gcc
 cd build-gcc
 ../gcc-14.2.0/configure \
-    --prefix=$BOOTSTRAP_PREFIX \
-    --with-sysroot=$SYSROOT \
-    --target=$TARGET \
+    --prefix="$STAGE1_PREFIX" \
+    --with-sysroot="$STAGE2_SYSROOT" \
+    --target="$TARGET" \
     --with-glibc-version=2.40 \
     --with-newlib \
     --without-headers \
@@ -53,26 +55,47 @@ cd build-gcc
     --disable-libvtv \
     --disable-libstdcxx \
     --disable-bootstrap \
-    --with-gmp=$BOOTSTRAP_PREFIX \
+    --with-gmp="$STAGE1_PREFIX" \
     --enable-languages=c,c++
 make -j10
 make install
 cd ..
 
 cd linux-6.12.8
-make ARCH=arm64 INSTALL_HDR_PATH=$SYSROOT headers_install
+make ARCH=arm64 INSTALL_HDR_PATH="$STAGE2_SYSROOT/usr" headers_install
 cd ..
 
 mkdir build-glibc
 cd build-glibc
 ../glibc-2.40/configure \
-    --prefix=$SYSROOT \
-    --host=$TARGET \
-    --with-headers=$SYSROOT/include \
+    --prefix="$STAGE2_SYSROOT/usr" \
+    --host="$TARGET" \
+    --with-headers="$STAGE2_SYSROOT/usr/include" \
     --enable-kernel=4.19 \
     --disable-multilib \
-    CC="$BOOTSTRAP_PREFIX/bin/aarch64-unknown-linux-gnu-gcc" \
-    CXX="$BOOTSTRAP_PREFIX/bin/aarch64-unknown-linux-gnu-g++"
+    CC="$STAGE1_PREFIX/bin/aarch64-unknown-linux-gnu-gcc" \
+    CXX="$STAGE1_PREFIX/bin/aarch64-unknown-linux-gnu-g++"
+make -j10
+make install
+cd ..
+
+mkdir build-libstdc++
+cd build-libstdc++
+../gcc-14.2.0/libstdc++-v3/configure \
+    --host="$TARGET" \
+    --prefix="$STAGE2_SYSROOT/usr" \
+    --disable-multilib \
+    --disable-nls \
+    --with-gxx-include-dir="$STAGE1_PREFIX/$TARGET/include/c++/14.2.0" \
+    CFLAGS="--sysroot='$STAGE2_SYSROOT'" \
+    CC="$STAGE1_PREFIX/bin/aarch64-unknown-linux-gnu-gcc" \
+    CXX="$STAGE1_PREFIX/bin/aarch64-unknown-linux-gnu-g++" \
+    AS="$STAGE1_PREFIX/aarch64-unknown-linux-gnu/bin/as" \
+    AR="$STAGE1_PREFIX/aarch64-unknown-linux-gnu/bin/ar" \
+    NM="$STAGE1_PREFIX/aarch64-unknown-linux-gnu/bin/nm" \
+    OBJDUMP="$STAGE1_PREFIX/aarch64-unknown-linux-gnu/bin/objdump" \
+    STRIP="$STAGE1_PREFIX/aarch64-unknown-linux-gnu/bin/strip" \
+    RANLIB="$STAGE1_PREFIX/aarch64-unknown-linux-gnu/bin/ranlib"
 make -j10
 make install
 cd ..
